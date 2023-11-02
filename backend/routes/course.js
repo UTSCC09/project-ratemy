0ebf1553f4d2ba9eb2b07c555fdb5d748d0fdda7
Priyankdave1 <84639969+Priyankdave1@users.noreply.github.com>
2023-11-02 16:00:07 -0400

    sanitize input

--- backend/routes/course.js
+++ backend/routes/course.js
@@ -3,6 +3,16 @@ const db = require("../db");
 
 module.exports.post = async (req, res) => {
     const courseData = req.body;
+    const courseCode = req.body.courseCode;
+    if (courseCode.length != 6
+        || courseCode[0] < 'A' || courseCode[0] > 'Z'
+        || courseCode[1] < 'A' || courseCode[1] > 'Z'
+        || courseCode[2] < 'A' || courseCode[2] > 'Z'
+        || courseCode[3] < 'A' || courseCode[3] > 'D'
+        || courseCode[4] < '0' || courseCode[4] > '9'
+        || courseCode[5] < '0' || courseCode[5] > '9') {
+        return res.status(400).json({ error: 'Invalid course code.' });
+    }
     const professorName = req.body.professorName; 
   
     const existingCourse = await db.models.course.findOne({ courseCode: courseData.courseCode });
