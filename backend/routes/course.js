388af11649ee238854bcdb6cb4f1f7e218d2a97d
Priyankdave1 <84639969+Priyankdave1@users.noreply.github.com>
2023-11-03 11:38:02 -0400

    fix regex

--- backend/routes/course.js
+++ backend/routes/course.js
@@ -4,35 +4,34 @@ const db = require("../db");
 module.exports.post = async (req, res) => {
     const courseData = req.body;
     const courseCode = req.body.courseCode;
-    //regex to check if course code is valid
-    if (!courseCode.match(/[A-Z]{3}[A-D][0-9]{2}/)) {
+    if (!courseCode.match(/[A-Z]{3}[A-D1-4][0-9]{2}/)) {
         return res.status(400).json({ error: 'Invalid course code.' });
     }
-    const professorName = req.body.professorName; 
-  
+    const professorName = req.body.professorName;
+
     const existingCourse = await db.models.course.findOne({ courseCode: courseData.courseCode });
-  
+
     if (existingCourse) {
-      const professorExists = existingCourse.professorNames.some(professor => professor === professorName);
-  
-      if (!professorExists) {
-        existingCourse.professorNames.push({
-          name: professorName,
-        });
-  
-        const updatedCourse = await existingCourse.save();
-  
-        return res.status(200).json(updatedCourse);
-      } else {
-        return res.status(400).json({ error: 'Professor already exists in the course.' });
-      }
+        const professorExists = existingCourse.professorNames.some(professor => professor === professorName);
+
+        if (!professorExists) {
+            existingCourse.professorNames.push({
+                name: professorName,
+            });
+
+            const updatedCourse = await existingCourse.save();
+
+            return res.status(200).json(updatedCourse);
+        } else {
+            return res.status(400).json({ error: 'Professor already exists in the course.' });
+        }
     } else {
-      const Course = new db.models.course(courseData);
+        const Course = new db.models.course(courseData);
         Course.professorNames.push({
             name: professorName,
         });
-      const insertedCourse = await Course.save();
-      return res.status(200).json(insertedCourse);
+        const insertedCourse = await Course.save();
+        return res.status(200).json(insertedCourse);
     }
-  };
+};
 
