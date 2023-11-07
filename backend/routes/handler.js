d0b002be060be59a40805b778126c5375f382161
Priyankdave1 <84639969+Priyankdave1@users.noreply.github.com>
2023-11-07 18:26:10 -0500

    endpoints for reviews

--- backend/routes/handler.js
+++ backend/routes/handler.js
@@ -9,6 +9,14 @@ const router = express.Router();
 
 const user = require("./user");
 const course = require("./course");
+const review = require("./review");
+
+
+router.post("/api/review", review.postReview);
+router.get("/api/reviews", review.getReviews);
+router.get("/api/reviews/:courseCode", review.getReviewsByCourseCode);
+router.patch("/api/review/:id", review.patchReview);
+router.delete("/api/review/:id", review.deleteReview);
 router.post("/api/user", user.post);
 router.get("/api/user/:id", user.get);
 router.post("/api/reviews", review.postReview);
