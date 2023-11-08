94c8cb79ae5871278fc5f62a98d27bf924dcd297
alinabuzila <88336627+alinabuzila@users.noreply.github.com>
2023-11-08 14:55:41 -0500

    GET endpoints revies

--- backend/routes/handler.js
+++ backend/routes/handler.js
@@ -11,8 +11,6 @@ const review = require("./review");
 router.post("/api/reviews", review.postReview);
 router.get("/api/reviews/all", review.getReviews);
 router.get("/api/reviews/:id", review.getCourseReviews);
-router.patch("/api/reviews/:id", review.patchReview);
-router.delete("/api/reviews/:id", review.deleteReview);
 router.post("/api/user", user.post);
 router.get("/api/user/:id", user.get);
 router.post("/api/reviews", review.postReview);
