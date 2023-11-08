21f73d3041cef762ac093fbcdaa0b5b2db9f945d
alinabuzila <88336627+alinabuzila@users.noreply.github.com>
2023-11-08 12:16:48 -0500

    added param validations

--- backend/routes/handler.js
+++ backend/routes/handler.js
@@ -2,26 +2,24 @@ const express = require("express");
 
 const router = express.Router();
 
-// const db = require("../db");
-
+// import mongoose from 'mongoose';
 // const ObjectId = require("mongodb").ObjectId;
-
 
 const user = require("./user");
 const course = require("./course");
 const review = require("./review");
 
 
-router.post("/api/review", review.postReview);
+router.post("/api/reviews", review.postReview);
 router.get("/api/reviews", review.getReviews);
-router.get("/api/reviews/:courseCode", review.getReviewsByCourseCode);
-router.patch("/api/review/:id", review.patchReview);
-router.delete("/api/review/:id", review.deleteReview);
+router.get("/api/reviews/:courseId", review.getCourseReviews);
+router.patch("/api/reviews/:id", review.patchReview);
+router.delete("/api/reviews/:id", review.deleteReview);
 router.post("/api/user", user.post);
 router.get("/api/user/:id", user.get);
 router.post("/api/reviews", review.postReview);
-router.post("/api/course", course.post);
+router.post("/api/courses", course.post);
 router.get("/api/courses", course.getAll);
-router.get("/api/course/:id", course.get);
+router.get("/api/courses/:id", course.get);
 
 module.exports = router;
--- backend/routes/review.js
+++ backend/routes/review.js
@@ -21,3 +21,75 @@ exports.postReview = async (req, res) => {
         return res.status(500).json({ error: err.message });
     }
 }
+
+// takes page and limit as query params
+exports.getReviews = async (req, res) => {
+    const page = req.query.page || 0;
+    const limit = req.query.limit || 10;
+    try {
+        const reviews = await db.models.review.find().sort({ date: -1 }).skip(page * limit).limit(limit);
+        return res.status(200).json(reviews);
+    }
+    catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
+}
+
+// takes page and limit as query params
+exports.getCourseReviews = async (req, res) => {
+    const page = req.query.page || 0;
+    const limit = req.query.limit || 10;
+    const courseId = req.params.courseId;
+    if (courseId == null) {
+        return res.status(400).json({ error: 'Missing course id.' })
+    }
+    try {
+        const reviews = await db.models.review.find({ course_id: mongoose.Types.ObjectId(courseId) }).skip(page * limit).limit(limit);
+        console.log("here");
+        return res.status(200).json(reviews);
+    }
+    catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
+}
+
+exports.patchReview = async (req, res) => {
+    const reviewId = req.params.id;
+    if (reviewId == null) {
+        return res.status(400).json({ error: 'Missing review id' })
+    }
+    const reviewData = req.body;
+    try {
+        const existingReview = await db.models.review.findById(reviewId);
+        if (!existingReview) {
+            return res.status(404).json({ error: 'Review not found.' });
+        }
+        const updatedReview = await db.models.review.findOneAndUpdate(
+            { _id: reviewId },
+            reviewData,
+            { new: true }
+        );
+        return res.json(updatedReview);
+    }
+    catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
+}
+
+exports.deleteReview = async (req, res) => {
+    const reviewId = req.params.id;
+    if (reviewId == null) {
+        return res.status(400).json({ error: 'Missing review id' })
+    }
+    try {
+        const existingReview = await db.models.review.findById(reviewId);
+        if (!existingReview) {
+            return res.status(404).json({ error: 'Review not found.' });
+        }
+        const deletedReview = await db.models.review.findByIdAndDelete(reviewId);
+        return res.json(deletedReview);
+    }
+    catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
+}
