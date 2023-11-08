dd6383d99f8d327932781644c864a4b1dfc6af21
Priyankdave1 <84639969+Priyankdave1@users.noreply.github.com>
2023-11-08 18:02:29 -0500

    remove unecessary code

--- backend/server.js
+++ backend/server.js
@@ -60,12 +60,7 @@
     passport.deserializeUser((user, done) => {
     done(null, user);
     });
-    app.use(function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-        res.header("Access-Control-Allow-Headers", "Content-Type");
-        res.header("Access-Control-Allow-Methods", "*");
-        next();
-      });
+    
     app.use(require("./routes/handler"));
     app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
