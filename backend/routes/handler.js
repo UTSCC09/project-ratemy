656a47712488f77426017ccebee348aac7caad9a
Yuval Kamani <yuvalkamani@gmail.com>
2023-11-10 14:15:17 -0500

    Commit

--- backend/routes/handler.js
+++ backend/routes/handler.js
@@ -30,26 +30,19 @@ router.get("/api/user", secured, user.userData);
 const review = require("./review");
 
 
-router.post("/api/review", review.postReview);
-router.get("/api/reviews", review.getReviews);
-router.get("/api/reviews/:courseCode", review.getReviewsByCourseCode);
-router.patch("/api/review/:id", review.patchReview);
-router.delete("/api/review/:id", review.deleteReview);
+router.post("/api/reviews", review.postReview);
+router.get("/api/reviews/all", review.getReviews);
+router.get("/api/reviews/:id", review.getCourseReviews);
 router.post("/api/user", user.post);
 router.get("/api/user/:id", user.get);
 router.post("/api/reviews", review.postReview);
-router.post("/api/course", course.post);
 router.get("/api/auth/login", loginAuth, auth.login);
 router.get("/api/auth/callback", auth.callback);
 router.get("/api/auth/logout", auth.logout);
 
 const review = require("./review");
 
-
-router.post("/api/reviews", review.postReview);
-router.get("/api/reviews/all", review.getReviews);
-router.get("/api/reviews/:id", review.getCourseReviews);
-router.post("/api/reviews", review.postReview);
+router.post("/api/courses", course.post);
 router.get("/api/courses", course.getAll);
 router.get("/api/courses/:id", course.get);
 
