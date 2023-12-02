322ab8107c923459f70577ee1a4edfdba613823f
Priyankdave1 <84639969+Priyankdave1@users.noreply.github.com>
2023-12-01 23:07:33 -0500

    connected stripe and open ai

--- backend/routes/openAi.js
+++ backend/routes/openAi.js
@@ -7,41 +7,52 @@ const openaiApiKey = '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'; // R
 const openaiClient = new openai({ apiKey: openaiApiKey });
 const mongoose = require("mongoose");
 module.exports.post = async (req, res) => {
-  let prompt = req.body.question;
-  let courseId = req.body.courseId || null;
-  const professorFilter = req.body.professor ? { professor: req.body.professor } : {};
-  if (courseId == null) {
-      return res.status(400).json({ error: "Missing course id." });
-  }
+  const email = req.body.email;
+
   try {
-    // Check if the prompt is provided
-    if (!prompt) {
-      throw new Error("Uh oh, no prompt was provided");
+    // Check if the user is subscribed
+    const isSB = await db.models.isSubscribed.findOne({ email: email });
+
+    // Check OpenAI count
+    const openaiCountupdate = await db.models.OpenAiCount.findOne({ email: email });
+    if (!openaiCountupdate) {
+      const newOpenaiCount = await db.models.OpenAiCount.create({ email: email, count: 1 });
     }
+    if (!isSB && openaiCountupdate && openaiCountupdate.count === 3) {
+      return res.status(403).json({ error: "Not subscribed." });
+    }
+    const {courseId, professor } = req.body;
+    let prompt = req.body.question;
     
-      const reviews = await db.models.review
-        .find({ course_id: new mongoose.Types.ObjectId(courseId), ...professorFilter })
-        .limit(10);
-      
-      
-    
-      
-      // add reviews as string to prompt string before the prompt 
-      prompt ="These are reviews about a course: \n" +reviews+"\n"+prompt;
+    if (!courseId) {
+      return res.status(400).json({ error: "Missing course id." });
+    }
 
-      
-      const completion = await openaiClient.chat.completions.create({
-        messages: [{ role: "user", content: prompt }],
-        model: "gpt-3.5-turbo",
-      });
-
-      // Send the response from OpenAI to the frontend
-      res.json(completion.choices[0].message.content);
     
-  } catch (err) {
-    // Log the error and send a 500 Internal Server Error response
-    console.error(err);
-    res.status(500).json({ error: err.message });
+    if (!prompt) {
+      return res.status(400).json({ error: "Missing question." });
+    }
+
+    
+    const reviews = await db.models.review
+      .find({ course_id: new mongoose.Types.ObjectId(courseId), ...(professor ? { professor } : {}) })
+      .limit(10);
+
+    
+    prompt = `These are reviews about a course:\n${reviews}\n${prompt}`;
+
+    const completion = await openaiClient.chat.completions.create({
+      messages: [{ role: "user", content: prompt }],
+      model: "gpt-3.5-turbo",
+    });
+
+    if (openaiCountupdate) {
+      const updatedOpenaiCount = await db.models.OpenAiCount.updateOne({ email: email }, { count: openaiCountupdate.count + 1 });
+    }
+
+    res.json(completion.choices[0].message.content);
+  } catch (error) {
+    console.error("Error processing request:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-  
 };
